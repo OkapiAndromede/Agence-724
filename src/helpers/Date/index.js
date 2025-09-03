b905f0c4ce42d1125b693d40ba3c5c71a5d8e800
Neal Muller <neal.henri.muller@gmail.com>
2025-09-03 12:07:20 +0200

    docs : adding documentation for the function getMonth

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -1,3 +1,13 @@
+/**
+ * Retourne le mois en toutes lettre (français) à partir d'un objet Date.
+ *
+ * @param {Date} date : l'objet Date dont on veut en extraire le mois
+ * @returns {string} : le mois corespondant en toutes lettres
+ *
+ * @example
+ * getMonth(new Date("2022-01-01"));
+ * // -> "janvier"
+ */
 export const MONTHS = {
   1: "janvier",
   2: "février",
