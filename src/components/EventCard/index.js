97457f3d2645e0929d6f70d5e8556e5401476e73
Neal Muller <neal.henri.muller@gmail.com>
2025-09-05 14:14:25 +0200

    docs (EventCard) : adding JSdoc for the component

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -2,7 +2,21 @@ import PropTypes from "prop-types";
 import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
-
+/**
+ * Composant EventCard réutilisable affichant une carte d'évènement
+ *
+ * Rend une carte contenant une image, un label, un titre et une date formatée
+ * @component
+ *
+ * @param {string} imageSrc : la source de l'image
+ * @param {string} [imageAlt = "image"] : le titre alternatif de l'image
+ * @param {Date} [date = new Date()] : un objet Date représentant la date de l'évènement
+ * @param {string} title : le titre de l'évênement
+ * @param {string} label : le label associé à l'évènement
+ * @param {boolean} [small = false] : un booléen agissant sur le style de la carte
+ *
+ * @returns {JSX.Element} composant EventCard rendu
+ */
 const EventCard = ({
   imageSrc,
   imageAlt,
