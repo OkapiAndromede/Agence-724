712d17761cda72d4d52a64365ac5fba247863850
Neal Muller <neal.henri.muller@gmail.com>
2025-09-07 17:02:28 +0200

    docs (PeopleCard) : adding documentation for the component

--- src/components/PeopleCard/index.js
+++ src/components/PeopleCard/index.js
@@ -1,18 +1,30 @@
 import PropTypes from "prop-types";
 
 import "./style.scss";
-
+/**
+ * Composant PeopleCard affichant une carte d'un membre de l'équipe
+ *
+ * Rend une carte contenant une image, un nom et le poste de l'employé
+ * @component
+ *
+ * @param {string} imageSrc : la source de l'image
+ * @param {string} [imageAlt = ""] : le titre alternatif de l'image
+ * @param {string} position : le poste de l'employé
+ * @param {string} name : le nom de l'employé
+ *
+ * @returns {JSX.Element} composant PeopleCard rendu
+ */
 const PeopleCard = ({ imageSrc, imageAlt, position, name }) => (
-    <div className="PeopleCard">
-      <div className="PeopleCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-      </div>
-      <div className="PeopleCard__descriptionContainer">
-        <div className="PeopleCard__name">{name}</div>
-        <div className="PeopleCard__position">{position}</div>
-      </div>
+  <div className="PeopleCard">
+    <div className="PeopleCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
     </div>
-  );
+    <div className="PeopleCard__descriptionContainer">
+      <div className="PeopleCard__name">{name}</div>
+      <div className="PeopleCard__position">{position}</div>
+    </div>
+  </div>
+);
 
 PeopleCard.propTypes = {
   imageSrc: PropTypes.string.isRequired,
@@ -23,6 +35,6 @@ PeopleCard.propTypes = {
 
 PeopleCard.defaultProps = {
   imageAlt: "",
-}
+};
 
 export default PeopleCard;
