f49f45bbd1c7e7d643a7f4fbc8d3754ab90a132c
Neal Muller <neal.henri.muller@gmail.com>
2025-09-10 12:03:09 +0200

    docs (ServiceCard) : adding JSdoc for the component

--- src/components/ServiceCard/index.js
+++ src/components/ServiceCard/index.js
@@ -1,15 +1,26 @@
 import PropTypes from "prop-types";
 
 import "./style.scss";
-
+/**
+ * Composant ServiceCard affichant un service de l'agence
+ *
+ * Rend une carte contenant une image et l'enfant du composant
+ * @component
+ *
+ * @param {string} imageSrc : Source de l'image
+ * @param {string} [imageAlt = "image"] : Titre alternatif de l'image
+ * @param {React.ReactNode} children : Enfant du composant
+ *
+ * @returns {JSX.Element} Composant ServiceCard rendu
+ */
 const ServiceCard = ({ imageSrc, imageAlt, children }) => (
-    <div className="ServiceCard">
-      <div className="ServiceCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-      </div>
-      <div className="ServiceCard__textContainer">{children}</div>
+  <div className="ServiceCard">
+    <div className="ServiceCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
     </div>
-  );
+    <div className="ServiceCard__textContainer">{children}</div>
+  </div>
+);
 
 ServiceCard.propTypes = {
   imageSrc: PropTypes.string.isRequired,
@@ -18,7 +29,7 @@ ServiceCard.propTypes = {
 };
 
 ServiceCard.defaultProps = {
-  imageAlt: "image"
-}
+  imageAlt: "image",
+};
 
 export default ServiceCard;
