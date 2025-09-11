4bda56e2840399a55a54b93486e96c182876bee9
Neal Muller <neal.henri.muller@gmail.com>
2025-09-11 16:15:07 +0200

    refactor (filteredEvents) : separation of filter with 2 independant filter

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -6,6 +6,7 @@ import Modal from "../Modal";
 import ModalEvent from "../ModalEvent";
 
 import "./style.css";
+import { filterPagination, filterType } from "../../utils/array";
 /**
  * Composant EventList affichant une liste d'évènement
  *
@@ -29,27 +30,15 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  // const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
-  //   (event, index) => {
-  //     if (
-  //       (currentPage - 1) * PER_PAGE <= index &&
-  //       PER_PAGE * currentPage > index
-  //     ) {
-  //       return true;
-  //     }
-  //     return false;
-  //   }
-  // );
-  const filteredEvents = (data?.events || [])
-    .filter((event) => !type || event.type === type)
-    .filter(
-      (_, index) =>
-        index >= (currentPage - 1) * PER_PAGE && index < PER_PAGE * currentPage
-    );
+
+  const dataFiltered = filterType(data?.events, type);
+  const filteredEvents = filterPagination(dataFiltered, currentPage, PER_PAGE);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
