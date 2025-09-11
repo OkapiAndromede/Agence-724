198e0923dd85c12aff405c918bb6905f861b952a
Neal Muller <neal.henri.muller@gmail.com>
2025-09-11 16:40:50 +0200

    feat : adding filterType function and filterPagination function

--- src/utils/array.js
+++ src/utils/array.js
@@ -24,3 +24,39 @@ export const sortDescending = (data = []) => {
     return [];
   }
 };
+/**
+ * Fonction filterType appliquant un filtre par type d'évènement
+ * @function
+ *
+ * @param {Event[]} data : Tableau d'évènement à filtrer
+ * @param {string} type : Type d'évènement
+ * @returns {Event[]} Tableau d'évènement filtrés par type d'évènement
+ */
+export const filterType = (data, type) => {
+  try {
+    return (data || []).filter((event) => !type || event.type === type);
+  } catch (err) {
+    console.log(`Erreur lors du filtre des évènements par type : ${err}`);
+    return [];
+  }
+};
+/**
+ * Fonction filterPagination appliquant un filtre par pagination
+ * @param {Event[]} data : Tableau d'évènement à filtrer
+ * @param {number} currentPage : La page actuelle
+ * @param {number} PER_PAGE : Nombre maximum d'évènement par page
+ * @returns {Event[]} Tableau d'évènement filtrés par pagination
+ */
+export const filterPagination = (data, currentPage, PER_PAGE) => {
+  try {
+    return (data || []).filter(
+      (_, index) =>
+        index >= (currentPage - 1) * PER_PAGE && index < PER_PAGE * currentPage
+    );
+  } catch (err) {
+    console.log(
+      `Erreur lors du filtrage des évènements par pagination : ${err}`
+    );
+    return [];
+  }
+};
