093eb5016164136b1d7ff40f43720513ac0ce609
Neal Muller <neal.henri.muller@gmail.com>
2025-09-12 14:37:45 +0200

    refactor : deleting unnecessary describe that he was present two times

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -6,14 +6,12 @@ import { getMonth } from "../Date/index";
  */
 
 describe("Date helper", () => {
-  describe("When getMonth is called", () => {
-    it("the function return janvier for 2022-01-01 as date", () => {
-      const date = new Date("2022-01-01");
-      expect(getMonth(date)).toBe("janvier");
-    });
-    it("the function return juillet for 2022-07-08 as date", () => {
-      const date = new Date("2022-07-08");
-      expect(getMonth(date)).toBe("juillet");
-    });
+  it("the function return janvier for 2022-01-01 as date", () => {
+    const date = new Date("2022-01-01");
+    expect(getMonth(date)).toBe("janvier");
+  });
+  it("the function return juillet for 2022-07-08 as date", () => {
+    const date = new Date("2022-07-08");
+    expect(getMonth(date)).toBe("juillet");
   });
 });
